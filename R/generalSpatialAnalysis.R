fc8f6c86e964b3de360fa08d6103176b76123a86
Kyle Taylor <ktaylora@users.noreply.github.com>
2015-07-12 17:22:40 -0600

    Update generalSpatialAnalysis.R

--- R/generalSpatialAnalysis.R
+++ R/generalSpatialAnalysis.R
@@ -76,7 +76,7 @@ rasterToPolygons <- function(r=NULL, method='gdal'){
 extractDensities <- function(x,s=5,d=15, p=c(0.5,0.9)){
   p <- sprintf("%.2f", round(as.numeric(p),2)) # force a trailing N
   # extract  range contours for raster surface x
-  q <- as.character(seq(0,1,0.05))
+  q <- sprintf("%.2f",as.numeric(seq(0,1,0.05))) 
     if(sum(as.character(p) %in% as.character(q)) != length(p)) stop("quantiles are typically extracted in 0.05 interval steps")
   # smooth
   smoothed <- gaussianSmoothing(x,s=s)
