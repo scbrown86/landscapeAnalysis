a20890a7158a53a8394a0db715b98e801a1ad44b
ktaylora <kyle.a.taylor@gmail.com>
2015-08-10 12:48:15 -0600

    house keeping

--- R/generalSpatialAnalysis.R
+++ R/generalSpatialAnalysis.R
@@ -63,13 +63,15 @@
 #
 
 rasterToPolygons <- function(r=NULL, method='gdal'){
+ cleanUp <- function(n){ unlink(paste(n,c("shp","xml","shx","prj","dbf"),sep="."),force=T,recursive=T) }
  if(grepl(method,pattern='gdal')){
    r_name=deparse(substitute(r))
      r_name=paste(r_name,sprintf("%.0f", round(as.numeric(runif(n=1,min=0,max=99999)))),sep="_")
    raster::writeRaster(r,paste(r_name,"tif",sep="."),overwrite=T);
-   unlink(paste(r_name,c("shp","xml","shx","prj","dbf"),sep="."))
+   cleanUp(r_name)
    if(try(system(paste(.getPythonPath(),.getGDALtoolByName("gdal_polygonize"),"-8",paste(r_name,"tif",sep="."),"-f \"ESRI Shapefile\"",paste(r_name,"shp",sep="."),sep=" ")))==0){
      if(class(try(s<-rgdal::readOGR(".",r_name,verbose=F))) != "try-error"){
+       cleanUp(r_name)
        return(s);
      } else {
        warning("gdal_polygonize error : it's possible we tried to polygonize a raster with only NA values")
