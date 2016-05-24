cb867a0f6d3a869751767d4a9e7492e8fcadee2e
Kyle Taylor <ktaylora@users.noreply.github.com>
2016-05-24 16:26:40 -0600

    Update landscapeMetrics.R

--- R/landscapeMetrics.R
+++ R/landscapeMetrics.R
@@ -19,7 +19,7 @@ spatialPolygonsToGrid <- function(s=NULL,n=NULL,area=NULL,type="hexagonal"){
   .include('rgdal')
   .include('rgeos')
   # sanity checks
-  if(is.null(s) | is.null(n)) { cat(" -- error: no values specified for s= and n=\n"); stop(); }
+  if(is.null(s) && is.null(n)) { cat(" -- error: no values specified for s= and n=\n"); stop(); }
   if(!is.null(area)){ # did the user define an area (in meters) that we can use to define the number of cells in our mesh grid?
     n <- ceiling(rgeos::gArea(spTransform(s,CRS(projection("+init=epsg:2163"))))/area)
   }
